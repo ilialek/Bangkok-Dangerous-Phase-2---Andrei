a0949ff819fa497a1c480d65aa4bc24f322545a5
Andrei Dumitru <dumitrudumi605@yahoo.com>
2026-03-25 12:07:11 +0100

    GitIgnore

--- Bangkok Dangerous Unity/Assets/Scripts/NPCNavigation/RoamZone.cs
+++ Bangkok Dangerous Unity/Assets/Scripts/NPCNavigation/RoamZone.cs
@@ -30,7 +30,7 @@ public class RoamZone : MonoBehaviour
     private void OnDrawGizmos()
     {
         BoxCollider col = GetComponent<BoxCollider>();
-        Gizmos.color = Color.darkOrange;
+        Gizmos.color = Color.orange;
         Gizmos.matrix = transform.localToWorldMatrix;
         Gizmos.DrawWireCube(col.center, col.size);
     }
